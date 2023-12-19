8314e532bb4d7954a19adc95992b78436d45bb25
Riky Saepuloh <dikysaepuloh@gmail.com>
2023-12-19 14:56:28 +0700

    add colors, drawable asset, HomeFragment progress, LocationFragment progress.

--- app/src/main/java/com/adhit/projectcapstone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/adhit/projectcapstone/ui/home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.R
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.adhit.projectcapstone.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -25,11 +29,22 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val searchView: View = binding.searchView
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val searchEditText: EditText = binding.searchView.findViewById(R.id.search_src_text)
+
+
+
+//        val etSearchView = binding.searchView.findViewById<>()
+//        searchEditText.setHintTextColor(ContextCompat.getColor(requireContext(),R.color.primaryColor))
+
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/adhit/projectcapstone/ui/location/LocationFragment.kt
+++ app/src/main/java/com/adhit/projectcapstone/ui/location/LocationFragment.kt
@@ -7,12 +7,22 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.adhit.projectcapstone.R
 import com.adhit.projectcapstone.databinding.FragmentLocationBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class LocationFragment : Fragment() {
+class LocationFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentLocationBinding? = null
     private val binding get() = _binding!!
+    private lateinit var googleMap: GoogleMap
+    private lateinit var mapView: MapView
+    private val defaultLocation = LatLng(37.7749, -122.4194)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,15 +35,59 @@ class LocationFragment : Fragment() {
         _binding = FragmentLocationBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val searchView: View = binding.searchView
-        val textView: TextView = binding.textLocation
-        locationViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textLocation
+//        locationViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(gMap: GoogleMap) {
+        googleMap = gMap
+
+        // Customize the map as needed
+//        googleMap.isMyLocationEnabled = true
+        googleMap.uiSettings.isZoomControlsEnabled = false
+        googleMap.uiSettings.isCompassEnabled = false
+        googleMap.uiSettings.isMyLocationButtonEnabled = false
+        googleMap.uiSettings.isMapToolbarEnabled = false
+
+        // Set the default location with a marker
+        setMarker(defaultLocation)
+    }
+
+    // Method to set a marker at a specific location
+    private fun setMarker(location: LatLng) {
+        googleMap.addMarker(MarkerOptions().position(location).title(""))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
